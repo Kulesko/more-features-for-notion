7348273dc24750b51cb8eb4eeb80929158a33e99
Kulesko <thegreg@freenet.de>
2022-08-23 12:16:55 +0200

    feat(board dividers): provide divider styles matching light and dark mode

--- src/contentScripts/content-script.js
+++ src/contentScripts/content-script.js
@@ -2,6 +2,8 @@
 
 (() => {
 
+    let isDarkMode;
+
     function processBoard(board) {
         let highlightable = board.querySelectorAll('.notion-collection-item div[data-content-editable-leaf]');
 
@@ -10,8 +12,16 @@
                 if (e.textContent.startsWith('==')) {
                     e.textContent = e.textContent.substring(2).trim();
                     var backgroundHolder = e.closest('.notion-page-block').firstElementChild;
-                    if (!!backgroundHolder)
-                        backgroundHolder.style.backgroundColor = 'rgba(191,191,190,.15)';
+                    if (!!backgroundHolder) {
+                        if (!isDarkMode) {
+                            backgroundHolder.style.background = 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 17px, rgba(248,245,241,1) 17px, rgba(248,245,241,1) 100%)';
+                        }
+                        else {
+                            backgroundHolder.style.background = '#191919';
+                            // box-shadow makes the dark backdrop too blurry
+                            backgroundHolder.style.boxShadow = null;
+                        }
+                    }
                 }
             });
         }
@@ -19,6 +29,8 @@
 
 
     function processPage() {
+        isDarkMode = document.querySelector('.notion-dark-theme');
+
         let boards = document.querySelectorAll('.notion-board-view');
         if (!!boards) {
             boards.forEach(b => processBoard(b));
