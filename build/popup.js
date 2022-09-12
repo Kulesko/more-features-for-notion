27ba21cb803b95bd68fdefb5b72619dc792134d6
Kulesko <thegreg@freenet.de>
2022-09-12 06:09:00 +0200

    refactor(popup): wrap all method definitions

--- build/popup.js
+++ build/popup.js
@@ -1,48 +1,53 @@
-function validate() {
+"use strict";
 
-    let tokenHolder = document.getElementById('api-key');
-    let apiToken = tokenHolder.value;
+(() => {
 
+    function validate() {
 
-    chrome.storage.sync.get(['apiToken'], function (result) {
-        console.log('Value currently is ' + result['apiToken']);
-        (async () => {
-
-            const Http = new XMLHttpRequest();
-            Http.open("GET", 'https://api.notion.com/v1/users/me');
-            Http.setRequestHeader('Authorization', 'Bearer ' + result['apiToken']);
-            Http.setRequestHeader('Notion-Version', '2022-06-28');
-            Http.send();
-            Http.onreadystatechange = (e) => {
-                console.log("success");
-                chrome.storage.sync.set({"apiToken": apiToken}, function () {
-                    console.log('Api token saved to storage');
-                });
-                document.getElementById('form-private-token').style = 'display: none';
-            };
-            Http.onerror = (e) => {
-                console.log("failure");
-                console.log(Http.responseText);
-                document.getElementById('label-token').style = 'color:red';
-            };
-            console.log("validating key")
-        })();
-    });
+        let tokenHolder = document.getElementById('api-key');
+        let apiToken = tokenHolder.value;
 
 
+        chrome.storage.sync.get(['apiToken'], function (result) {
+            console.log('Value currently is ' + result['apiToken']);
+            (async () => {
 
-    tokenHolder.value = '';
-    return false;
-}
+                const Http = new XMLHttpRequest();
+                Http.open("GET", 'https://api.notion.com/v1/users/me');
+                Http.setRequestHeader('Authorization', 'Bearer ' + result['apiToken']);
+                Http.setRequestHeader('Notion-Version', '2022-06-28');
+                Http.send();
+                Http.onreadystatechange = (e) => {
+                    console.log("success");
+                    chrome.storage.sync.set({"apiToken": apiToken}, function () {
+                        console.log('Api token saved to storage');
+                    });
+                    document.getElementById('form-private-token').style = 'display: none';
+                };
+                Http.onerror = (e) => {
+                    console.log("failure");
+                    console.log(Http.responseText);
+                    document.getElementById('label-token').style = 'color:red';
+                };
+                console.log("validating key")
+            })();
+        });
 
-function init() {
-    document.getElementById('form-private-token').onsubmit = validate;
-    document.getElementById('form-private-token').style = 'display: none';
-    chrome.storage.sync.get(['apiToken'], function(result) {
-        if(!result){
-            document.getElementById('form-private-token').style = null;
-        }
-    });
-}
 
-window.onload = init;
+        tokenHolder.value = '';
+        return false;
+    }
+
+    function init() {
+        document.getElementById('form-private-token').onsubmit = validate;
+        document.getElementById('form-private-token').style = 'display: none';
+        chrome.storage.sync.get(['apiToken'], function (result) {
+            if (!result) {
+                document.getElementById('form-private-token').style = null;
+            }
+        });
+    }
+
+    window.onload = init;
+
+})();
